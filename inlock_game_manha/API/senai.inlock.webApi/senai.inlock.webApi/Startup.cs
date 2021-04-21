5f9e537fad77c36aa19f7c89db115926ff41d0b7
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2021-04-20 21:58:01 -0300

    Swagger adicionado e problema resolvido

--- inlock_game_manha/API/senai.inlock.webApi/senai.inlock.webApi/Startup.cs
+++ inlock_game_manha/API/senai.inlock.webApi/senai.inlock.webApi/Startup.cs
@@ -4,9 +4,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace senai.inlock.webApi
@@ -50,6 +53,17 @@ namespace senai.inlock.webApi
 
                     };
                 });
+            
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "inlock.webApi", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +73,16 @@ namespace senai.inlock.webApi
             {
                 app.UseDeveloperExceptionPage();
             }
+           
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "inlock.webApi");
+            });
 
             app.UseRouting();
             //Habilita a autenticação(o usuário estar logado ou não) 
