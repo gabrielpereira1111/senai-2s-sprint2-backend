3c4194c577355c202bcaab38b9370dc3e7d12ac3
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2021-06-25 10:39:55 -0300

    Adiciona política de CORS mobile

--- SPMedGroup/senai.spmed.webApi/senai.spmed.webApi/Startup.cs
+++ SPMedGroup/senai.spmed.webApi/senai.spmed.webApi/Startup.cs
@@ -56,7 +56,7 @@ namespace senai.spmed.webApi
                 options.AddPolicy("CorsPolicy", 
                     builder =>
                     {
-                        builder.WithOrigins("http://localhost:3000")
+                        builder.WithOrigins("http://localhost:3000", "http://localhost:19006")
                                .AllowAnyHeader()
                                .AllowAnyMethod();
                     }
